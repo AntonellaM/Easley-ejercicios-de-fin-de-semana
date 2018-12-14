0b22e7d4b0e0e616cc1ff7a053238c6135904b23
Natalia PN <npelegero@gmail.com>
2018-12-14 18:29:06 +0100

    Add two parts of exercise, still have to review 2nd part.

--- js/main.js
+++ js/main.js
@@ -22,3 +22,48 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const ulContainer = document.querySelector('.news');
+
+for (let i = 0; i<data.length; i++) {
+
+  const titles = data[i].title;
+
+  const images = data[i].image;
+
+  ulContainer.innerHTML += `<li class="news__item"><h2 class="news__title">${titles}</h2>
+
+  <img src="${images}" alt="${titles}" class="news__image"></li>`;
+}
+
+
+const liElement = document.querySelectorAll('.news__item');
+
+const liChildren = document.querySelectorAll('.news__title');
+
+
+for (let i = 0; i<liElement.length; i++) {
+
+  if(liChildren[i].innerHTML.includes('Mars')) {
+
+    liChildren[i].parentElement.classList.add('news__item--from-mars');
+
+    console.log(liElement);
+
+  } else if (liChildren[i].innerHTML.includes('Martians')) {
+
+    liChildren[i].parentElement.classList.add('news__item--from-mars');
+
+    console.log(liElement);
+
+  }
+}
+
+
+
+ 
+// Recorreremos la lista de elementos almacenando en una variable el contenido del título de cada elemento.
+// Usando el método includes() comprobaremos si contienen “Mars” o “Martian” y aplicaremos al licorrespondiente la clase .news__item--from-mars
+// Tada!!
+
+
