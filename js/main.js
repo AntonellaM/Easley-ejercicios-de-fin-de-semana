e61761900db5041eb723041e712600bcd47a8bcb
lilia <liliaborrazas14@gmail.com>
2018-12-15 00:27:21 +0100

    done ej 1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,15 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+const list = document.querySelector('.news');
+
+let info = [];
+for(let i =0; i<data.length; i++){
+
+  info+=`<li class="news__item">
+         <h2 class="news__title">${data[i].title}</h2>
+         <img class="news__image" src=${data[i].image} alt=${data[i].title}>
+         </li>`
+};
+list.innerHTML = info;
+
