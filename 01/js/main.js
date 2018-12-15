377cde70436837fd414ca3397089dc7926a0b070
pixelvega <pixelvega.info@gmail.com>
2018-12-16 00:38:19 +0100

    folders structure and exercise 01

---
+++ 01/js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+console.log('ejercicio 1');
+
+const data = [
+  {
+    title: 'Asteroids 101',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Life on Mars',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Martians invade Earth',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Humans aren\'t real',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Space The Final Frontier',
+    image: 'https://via.placeholder.com/200x100'
+  }
+];
+
+const ul = document.querySelector('.news');
+for (let i= 0; i<data.length;i++){
+	ul.innerHTML+= 
+	`<li class="news__item">
+		<h2 class="news__title">${data[i].title}</h2>
+		<img class="news__image" src="${data[i].image}" alt="${data[i].title}">
+	</li>`;
+}
