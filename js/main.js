7a5575d733ebc01ef85a90e706654353e2ef0c5e
Natalia PN <npelegero@gmail.com>
2018-12-15 12:31:18 +0100

    Finnish 3rd part of exercise

--- js/main.js
+++ js/main.js
@@ -1,70 +1,87 @@
 'use strict';
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
+// const data = [
+//   {
+//     title: 'Asteroids 101',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Life on Mars',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Martians invade Earth',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Humans aren\'t real',
+//     image: 'https://via.placeholder.com/200x100'
+//   },
+//   {
+//     title: 'Space The Final Frontier',
+//     image: 'https://via.placeholder.com/200x100'
+//   }
+// ];
+
+// const ulContainer = document.querySelector('.news');
+
+// for (let i = 0; i<data.length; i++) {
+
+//   const titles = data[i].title;
+
+//   const images = data[i].image;
+
+//   ulContainer.innerHTML += `<li class="news__item"><h2 class="news__title">${titles}</h2>
+
+//   <img src="${images}" alt="${titles}" class="news__image"></li>`;
+// }
+
+
+// const liElement = document.querySelectorAll('.news__item');
+
+// const liChildren = document.querySelectorAll('.news__title');
+
+
+
+// for (let i = 0; i<liElement.length; i++) {
+
+//   if(liChildren[i].innerHTML.includes('Mars')) {
+
+//     liChildren[i].parentElement.classList.add('news__item--from-mars');
+
+//     console.log(liElement);
+
+//   } else if (liChildren[i].innerHTML.includes('Martians')) {
+
+//     liChildren[i].parentElement.classList.add('news__item--from-mars');
+
+//     console.log(liElement);
+
+//   }
+// }
+
+
+
+// 4th part///
+
+
 
 const ulContainer = document.querySelector('.news');
 
-for (let i = 0; i<data.length; i++) {
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
 
-  const titles = data[i].title;
+  .then(r=>r.json())
+  .then(d=> {
+    const newsResults = d.news;
 
-  const images = data[i].image;
+    for (let i = 0; i<newsResults.length; i++) {
 
-  ulContainer.innerHTML += `<li class="news__item"><h2 class="news__title">${titles}</h2>
+      const titles = newsResults[i].title;
 
-  <img src="${images}" alt="${titles}" class="news__image"></li>`;
-}
+      const images = newsResults[i].image;
 
+      ulContainer.innerHTML += `<li class="news__item"><h2 class="news__title">${titles}</h2>
 
-const liElement = document.querySelectorAll('.news__item');
-
-const liChildren = document.querySelectorAll('.news__title');
-
-
-
-for (let i = 0; i<liElement.length; i++) {
-
-  if(liChildren[i].innerHTML.includes('Mars')) {
-
-    liChildren[i].parentElement.classList.add('news__item--from-mars');
-
-    console.log(liElement);
-
-  } else if (liChildren[i].innerHTML.includes('Martians')) {
-
-    liChildren[i].parentElement.classList.add('news__item--from-mars');
-
-    console.log(liElement);
-
-  }
-}
-
-
-
- 
-// Recorreremos la lista de elementos almacenando en una variable el contenido del título de cada elemento.
-// Usando el método includes() comprobaremos si contienen “Mars” o “Martian” y aplicaremos al licorrespondiente la clase .news__item--from-mars
-// Tada!!
-
-
+      <img src="${images}" alt="${titles}" class="news__image"></li>`;
+    }
+  });
