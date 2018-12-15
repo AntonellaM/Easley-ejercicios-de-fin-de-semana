40ca85586269190277d26ca89027cd306e3a419e
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2018-12-15 21:44:35 +0100

    fourth

--- 4_/js/main.js
+++ 4_/js/main.js
@@ -1,24 +1,52 @@
 'use strict';
+const ulContent = document.querySelector('.news');
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
+const handleContent = (event) => {
+  const selected = event.target;
+  // console.log(selected);
+  // console.log(event);
+  if (event.target.tagName === 'LI') {
+    selected.classList.remove('news__item--no-image-visible');
+  } else {
+    const parent = selected.parentElement;
+    parent.classList.remove('news__item--no-image-visible');
   }
-];
+};
+
+const fetchApp = () => {
+  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      const allData = data.news;
+
+      for (let listData of allData) {
+
+        const newList = document.createElement('li');
+        newList.classList.add('news__item', 'news__item--no-image-visible');
+
+        const newTitle = document.createElement('h2');
+        const eachTitleText = document.createTextNode(listData.title);
+        newTitle.appendChild(eachTitleText);
+        newTitle.classList.add('news__title');
+
+        const newImg = document.createElement('img');
+        newImg.classList.add('news__image');
+        newImg.setAttribute('src', listData.image);
+        newImg.setAttribute('alt', listData.title);
+
+        newList.appendChild(newTitle);
+        newList.appendChild(newImg);
+
+        ulContent.appendChild(newList);
+
+        const titleEl = listData.title;
+        if (titleEl.includes('Mars') || titleEl.includes('Martian')) {
+          newTitle.classList.add('news__item--from-mars');
+        }
+      }
+    });
+};
+
+fetchApp();
+ulContent.addEventListener('click', handleContent);
