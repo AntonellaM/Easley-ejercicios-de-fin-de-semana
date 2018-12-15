6ab1c73d7dc0e852fd6736b0b39efa8627c31ff2
renrith <sandrafernfer@gmail.com>
2018-12-15 09:49:28 +0100

    finish exercises 1 and 2

--- js/main.js
+++ js/main.js
@@ -22,3 +22,39 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+// Ejercicio 1
+
+const newsList = document.querySelector('.news');
+
+for(let ii = 0; ii < data.length; ii++) {
+  const newLi = document.createElement('li');
+  const newH2 = document.createElement('h2');
+  const newImg = document.createElement('img');
+
+  newLi.classList.add('news__item');
+  newH2.classList.add('news_title');
+  newImg.classList.add('news_image');
+
+  const newsTitle = data[ii].title;
+  const newsImage = data[ii].image;
+
+  newImg.src = newsImage;
+  newImg.setAttribute('alt', newsTitle);
+
+  newH2.appendChild(document.createTextNode(newsTitle));
+  newLi.appendChild(newH2);
+  newLi.appendChild(newImg);
+
+  newsList.appendChild(newLi);
+}
+
+// Ejercicio 2
+const newsItem = document.querySelectorAll('.news__item');
+
+for(let ii = 0; ii < newsItem.length; ii++) {
+
+  if(newsItem[ii].innerHTML.includes('Mars') || newsItem[ii].innerHTML.includes('Martian')){
+    newsItem[ii].classList.add('news__item--from-mars');
+  }
+}
