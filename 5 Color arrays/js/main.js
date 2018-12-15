a6387f54238179f507aa5d2a4ad34b17b2bc2418
emma-martin <emmamartinalvarez@gmail.com>
2018-12-15 18:52:06 +0100

    Set ex4 structure

--- 5 Color arrays/js/main.js
+++ 5 Color arrays/js/main.js
@@ -8,3 +8,58 @@
 // <div class="color__item" style="background-color:#ffcc00"></div>
 // De esta manera aplicaremos el color a cada cuadrado.
 
+//necesito un div que contenga un h2, un texto y otro div donde van dentro los colores.
+//necesito un fetch para sacar palettes. tenemos un objeto con un key(palettes). Ese key tiene como valor un array. El array tiene un objeto. Ese objeto tiene tres keys. Quiero name, from y colors. Colors ademas como valor tiene otro array con 5 elementos.
+const palettes = document.querySelector('.palettes');
+
+function createElement(tag, classEl){
+  const newElement = document.createElement(tag);
+  newElement.classList.add(classEl);
+  return newElement;
+}
+
+function createText(text){
+  const newText = document.createTextNode(text);
+
+  return newText;
+}
+
+function appendText(tag, classEl, text){
+  const newElCreated = createElement(tag, classEl);
+  const newTextCreated = createText(text);
+  newElCreated.appendChild(newTextCreated);
+  return newElCreated;
+}
+
+function createColorBox(colorList){
+  const ulBox = createElement('ul', 'ul-wrapper');
+  for(const color of colorList){
+    let liBox = createElement('li', 'li-color');
+    let divBox = createElement('div', 'div-color');
+    liBox.appendChild(divBox);
+    divBox.style.backgroundColor = color;
+    ulBox.appendChild(liBox);
+  }
+  return ulBox;
+}
+
+
+function createPalette(name, from, colors){
+  const newName = appendText('h2', 'title', name);
+  const newFrom = appendText('p', 'from', from);
+  const newColorBox = createColorBox(colors);
+  console.log(newName, newFrom, newColorBox);
+  palettes.appendChild(newName);
+  palettes.appendChild(newFrom);
+  palettes.appendChild(newColorBox);
+}
+
+createPalette('emma', 'pakito', ['green', 'yellow']);
+
+
+
+// fetch('https://raw.githubusercontent.com/Adalab/dorcas-repaso-sprint2/master/data/palettes.json')
+//   .then(response => response.json())
+//   .then(function(data){
+
+//   })
