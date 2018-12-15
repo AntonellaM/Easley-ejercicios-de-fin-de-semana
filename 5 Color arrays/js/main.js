69ee9ea1f5c8eb57dcc5185c6aa3d74d56a8c918
emma-martin <emmamartinalvarez@gmail.com>
2018-12-15 17:01:37 +0100

    Set up 5th exercise

---
+++ 5 Color arrays/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Esta vez tenemos una URL https://raw.githubusercontent.com/Adalab/dorcas-repaso-sprint2/master/data/palettes.json
+//que nos devuelve datos de una paleta y tenemos que pintarla como en el ejemplo:
+// Pero hay uno requisito:
+
+// Podemos crear los estilos que estimemos oportunos pero el color de la paleta lo añadiremos con un atributo style="" en cada cuadrado, es decir, si cada cuadrado fuese un div podría quedar algo así:
+// <div class="color__item" style="background-color:#ffcc00"></div>
+// De esta manera aplicaremos el color a cada cuadrado.
+
