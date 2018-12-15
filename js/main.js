8f0217012d8ca4eb52f76920c6fc6c98feaf17ee
renrith <sandrafernfer@gmail.com>
2018-12-15 11:27:23 +0100

    add all finished exercises

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ for(let ii = 0; ii < newsItem.length; ii++) {
   }
 }*/
 
-// Ejercicio 3
+// Ejercicio 3 y 4
 
 fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
   .then(response => response.json())
@@ -74,8 +74,9 @@ fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-sema
       const newImg = document.createElement('img');
 
       newLi.classList.add('news__item');
-      newH2.classList.add('news_title');
-      newImg.classList.add('news_image');
+      newLi.classList.add('news__item--no-image-visible'); //Ej 4
+      newH2.classList.add('news__title');
+      newImg.classList.add('news__image');
 
       const newsTitle = data[ii].title;
       const newsImage = data[ii].image;
@@ -87,6 +88,8 @@ fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-sema
       newLi.appendChild(newH2);
       newLi.appendChild(newImg);
 
+      newLi.addEventListener('click', toggleImage); //Ej 4
+
       newsList.appendChild(newLi);
     }
 
@@ -97,5 +100,56 @@ fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-sema
       if(newsItem[ii].innerHTML.includes('Mars') || newsItem[ii].innerHTML.includes('Martian')){
         newsItem[ii].classList.add('news__item--from-mars');
       }
+    }
+  });
+
+function toggleImage() { //Ej 4
+  this.classList.toggle('news__item--no-image-visible');
+}
+
+//Ejercicio 5
+
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+  .then(response => response.json())
+  .then(info => {
+    const palette = info.palettes[0].colors;
+    const paletteContainer = document.querySelector('.palette');
+
+    for(let ii = 0; ii < palette.length; ii++) {
+      const newLi = document.createElement('li');
+      newLi.classList.add('color__item');
+
+      const color = `#${palette[ii]}`;
+      newLi.style.backgroundColor = color;
+
+      paletteContainer.appendChild(newLi);
+    }
+  });
+
+//Ejercicio 6
+
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
+  .then(response => response.json())
+  .then(info => {
+    const palettes = info.palettes;
+    const palettesTogether = [];
+
+    for(let ii = 0; ii < palettes.length; ii++) {
+
+      for(let jj = 0; jj < palettes[ii].colors.length; jj++){
+        palettesTogether.push(palettes[ii].colors[jj]);
+      }
+    }
+
+    const palettesContainer = document.querySelector('.palettes');
+
+    for(let ii = 0; ii < palettesTogether.length; ii++) {
+      const newLi = document.createElement('li');
+      newLi.classList.add('color__item');
+
+      const color = `#${palettesTogether[ii]}`;
+      newLi.style.backgroundColor = color;
+
+      palettesContainer.appendChild(newLi);
     }
   });
