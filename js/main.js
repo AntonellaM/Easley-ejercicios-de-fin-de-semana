290e4bce89935ee7d0eb3aa7a736353b76477ca5
katia1802 <katia.rojas.sandoval@gmail.com>
2018-12-15 13:24:09 +0100

    Second Exercise

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const data = [
-  {
+const data = [{
     title: 'Asteroids 101',
     image: 'https://via.placeholder.com/200x100'
   },
@@ -23,3 +22,55 @@ const data = [
   }
 ];
 
+const listEl = document.querySelector('.news');
+// console.log (listEl);
+
+function createList() {
+  for (let i = 0; i < data.length; i++) {
+    const dataImages = data[i].image;
+    // console.log (dataImages);
+
+    const dataTitles = data[i].title;
+    // console.log (dataTitles);
+    listEl.innerHTML += `
+    <li class="news__item">
+    <h2 class="news__title">${dataTitles}</h2>
+    <img class="news__image" src=${dataImages} alt=${dataTitles}>
+    </li>`;
+  }
+}
+
+createList();
+
+const itemEl = document.querySelectorAll('.news__item');
+//  console.log (itemEl);
+
+function highlightList() {
+  for (let i = 0; i < itemEl.length; i++) {
+    const itemName = itemEl[i].outerText;
+    // console.log(itemName);
+
+    if (itemName.includes('Martians')||itemName.includes('Mars')){
+      itemEl[i].classList.add('news__item--from-mars');
+    } else {
+      console.log(`${itemName} doesnt include "Mars" nor "Martians"`);
+
+    }
+  }
+}
+
+highlightList();
+
+
+// function highlightList (){
+//   for (let i = 0; i<data.length; i++){
+//     const dataTitles = data[i].title;
+//     // console.log (dataTitles);
+
+//     if (dataTitles[i].includes('Mars')){
+//       itemEl.classList.add('news__item--from-mars','news__item--from-mars','news__item--from-mars');
+//     } else {
+//       // console.log (`${dataTitles} doesnt include Mars`);
+//     }
+//   }
+// }
