6fa582674a4427cbe82b6f179484145d13d2c57a
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2018-12-15 16:20:33 +0100

    first exercise

--- ejercicios/ejercicio1/js/main.js
+++ ejercicios/ejercicio1/js/main.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const newsElement = document.querySelector('.news');
+
+function printNews() {
+    fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/js/main.js')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (const newsItem of data) {
+            newsElement.innerHTML = `<li class="news__item">
+            <h2 class="news__title">${data.title}</h2>
+            <img class="news__image" src=${data.image}alt=${data.title}></li>`
+        }
+        });
+
+}
+printNews();
