eb8e0b1d57c216122d1de5510b466033f72f1130
maria-badanina <maria.badanina.s@gmail.com>
2018-12-15 23:28:33 +0100

    Marte, el planeta rojo

---
+++ ejercicios/Marte, el planeta rojo/js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const data = [
+  {
+    title: 'Asteroids 101',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Life on Mars',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Martians invade Earth',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Humans aren\'t real',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Space The Final Frontier',
+    image: 'https://via.placeholder.com/200x100'
+  }
+];
+
+const list = document.querySelector('.news');
+let newline;
+let newTitle;
+let titleContent;
+let newImage;
+let titleText;
+let wordMars = 'Mars';
+let wordMartians = 'Martians';
+
+for (const item of data) {
+  newline = document.createElement('li');
+  newline.setAttribute('class', 'news__item');
+  newTitle = document.createElement('h2');
+  newTitle.setAttribute('class', 'news__title');
+  titleContent = document.createTextNode(item.title);
+  newTitle.appendChild(titleContent);
+  newImage = document.createElement('img');
+  newImage.setAttribute('class', 'news__image');
+  newImage.setAttribute('alt', item.title);
+  newImage.setAttribute('src', item.image);
+  newline.appendChild(newTitle);
+  newline.appendChild(newImage);
+  list.appendChild(newline);
+  let sentence = newTitle.innerHTML;
+
+  if (sentence.includes('Mars')) {
+    newline.setAttribute('class', 'news__item--from-mars');
+  } else if (sentence.includes('Martians')) {
+    newline.setAttribute('class', 'news__item--from-mars');
+  }
+}
+
