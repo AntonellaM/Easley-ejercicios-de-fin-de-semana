d303198249bda0aab2c970d6542f25672647a4f5
renrith <sandrafernfer@gmail.com>
2018-12-15 10:25:16 +0100

    add exercise 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const data = [
+/*const data = [
   {
     title: 'Asteroids 101',
     image: 'https://via.placeholder.com/200x100'
@@ -50,6 +50,7 @@ for(let ii = 0; ii < data.length; ii++) {
 }
 
 // Ejercicio 2
+
 const newsItem = document.querySelectorAll('.news__item');
 
 for(let ii = 0; ii < newsItem.length; ii++) {
@@ -57,4 +58,44 @@ for(let ii = 0; ii < newsItem.length; ii++) {
   if(newsItem[ii].innerHTML.includes('Mars') || newsItem[ii].innerHTML.includes('Martian')){
     newsItem[ii].classList.add('news__item--from-mars');
   }
-}
+}*/
+
+// Ejercicio 3
+
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+  .then(response => response.json())
+  .then(info => {
+    const data = info.news;
+    const newsList = document.querySelector('.news');
+
+    for(let ii = 0; ii < data.length; ii++) {
+      const newLi = document.createElement('li');
+      const newH2 = document.createElement('h2');
+      const newImg = document.createElement('img');
+
+      newLi.classList.add('news__item');
+      newH2.classList.add('news_title');
+      newImg.classList.add('news_image');
+
+      const newsTitle = data[ii].title;
+      const newsImage = data[ii].image;
+
+      newImg.src = newsImage;
+      newImg.setAttribute('alt', newsTitle);
+
+      newH2.appendChild(document.createTextNode(newsTitle));
+      newLi.appendChild(newH2);
+      newLi.appendChild(newImg);
+
+      newsList.appendChild(newLi);
+    }
+
+    const newsItem = document.querySelectorAll('.news__item');
+
+    for(let ii = 0; ii < newsItem.length; ii++) {
+
+      if(newsItem[ii].innerHTML.includes('Mars') || newsItem[ii].innerHTML.includes('Martian')){
+        newsItem[ii].classList.add('news__item--from-mars');
+      }
+    }
+  });
