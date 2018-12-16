03b79233e8b6422fc097a7692e2a546c6ac34da7
Samanta Noblea <samantanoblea@gmail.com>
2018-12-16 12:00:28 +0100

    ex3

--- ejercicio 3/js/main.js
+++ ejercicio 3/js/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const newsList = document.querySelector('.news');
+const api = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json';
+
+fetch(api)
+  .then(r => r.json)
+  .then(d => {
+    const results = d.news;
+    for (let i=0; i<results.length; i++) {
+      const title = results[i].title;
+      const image = results[i].image;
+
+      const items = `<li class="news__item">
+  // <h2 class="news__title">${title}</h2>
+  // <img class="news__image" src=${image} alt=${title}>
+  // </li>`;
+      newsList.innerHTML += items;
+    }
+  });
+
+
+
+
+
+
+
+
+
+  // for (let i = 0; i < data.length; i++) {
+  //   const title = data[i].title;
+  //   const image = data[i].image;
+  //   const items = `<li class="news__item">
+  // <h2 class="news__title">${title}</h2>
+  // <img class="news__image" src=${image} alt=${title}>
+  // </li>`;
+  //   news.innerHTML += items;
+  // };
