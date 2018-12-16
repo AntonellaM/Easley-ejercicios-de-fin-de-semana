d7f1646cc745d3ece2925e16a792fc767cc26b46
pixelvega <pixelvega.info@gmail.com>
2018-12-16 23:37:48 +0100

    exercise 6 completed! :)

--- 06/js/main.js
+++ 06/js/main.js
@@ -1,25 +1,23 @@
 'use strict';
 console.log('ejercicio 6');
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
+const urlApi = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json';
+const container = document.querySelector('.palette__container');
+const paintPalettes = (para1) => {
+  fetch(para1)
+  .then(resp => resp.json())
+  .then(data => {
+    console.log(data);
+    const palettes = data.palettes;
+    //console.log(data.palettes);
+   for (let i=0;i<palettes.length;i++){
+     for (let j=0;j<palettes[i].colors.length;j++){
+        console.log(palettes[i].colors[j]);
+        console.log(container);
+        container.innerHTML += `<div class="item-palette" style="background-color:#${palettes[i].colors[j]}"></div>`;
+     }
+   }
+  });
+}
+
+paintPalettes(urlApi);
