b8d710e232d4689c84f16639bde910e29b536b34
Natalia PN <npelegero@gmail.com>
2018-12-16 16:28:14 +0100

    Finnish 5th part

---
+++ ejercicio-4/scripts/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+const palettesContainer = document.querySelector('.palettes__container');
+
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+
+  .then(response=>response.json())
+
+  .then(data=> {
+
+    const palettes = data.palettes[0].colors;
+
+    console.log(palettes);
+
+    for(let i = 0; i<palettes.length; i++) {
+
+      const colours = palettes[i];
+
+      const divsContent = `<div class="color__item" style="background-color:#${palettes[i]}"></div>`;
+
+      palettesContainer.innerHTML += divsContent;
+    }
+  });
+
