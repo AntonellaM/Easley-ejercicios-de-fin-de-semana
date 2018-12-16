a8af15b27959d1d14f0dc102df068f61dbc55a68
pixelvega <pixelvega.info@gmail.com>
2018-12-16 01:18:29 +0100

    exercise 2 completed

--- 02/js/main.js
+++ 02/js/main.js
@@ -23,3 +23,22 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const ul = document.querySelector('.news');
+
+for (let i = 0; i<data.length; i++){
+  ul.innerHTML += 
+	`<li class="news__item">
+		<h2 class="news__title">${data[i].title}</h2>
+		<img class="news__image" src="${data[i].image}" alt="${data[i].title}">
+  </li>`;
+
+  const liItems = document.querySelectorAll('.news__item');
+  
+  if(data[i].title.includes("Mars") || data[i].title.includes("Mart")){
+    console.log(data[i].title + ' incluye Mars');
+    liItems[i].classList.add('news__item--from-mars');
+  } else {
+    console.log(data[i].title + ' no incluye Mars');
+  }
+}
