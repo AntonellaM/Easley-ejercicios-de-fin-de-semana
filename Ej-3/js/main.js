68b2c79ed91f4e79a42f95ce4e49d7a7b3a2a3f5
desigc <desi.gc93@gmail.com>
2018-12-16 15:06:56 +0100

    Ex3

---
+++ Ej-3/js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+const newsEl = document.querySelector('.news');
+
+function printNews (){
+
+    fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+    .then(response => response.json())
+    .then(data => {
+     
+      const dataEl = data.news;
+      console.log('data', dataEl);
+      let listNews = '';
+      for (let i=0; i < dataEl.length; i++){
+        listNews += 
+        `<li class="news__item">
+          <h2 class="news__title">${dataEl[i].title}</h2>
+          <img class="news__image" src="${dataEl[i].image}" alt="${dataEl[i].title}">
+        </li>`;
+      }
+      newsEl.innerHTML = listNews;
+      importantNews();
+    });
+  
+}
+
+function importantNews (){
+  const listNews = document.querySelectorAll('.news__item');
+  console.log(listNews);
+  for (let i=0; i < listNews.length; i++){
+    const title = listNews[i].querySelector('.news__title').innerHTML;
+    
+    if(title.includes('Mars') || title.includes('Martian')){
+      listNews[i].classList.add('news__item--from-mars');
+    }
+  }
+  // for (const news of listNews){
+  //   const title = news.querySelector('.news__title').innerHTML;
+    
+  //   if(title.includes('Mars')||title.includes('Martians')){
+  //     news.classList.add('.news__item--from-mars');
+  //   }
+  // }
+  
+  
+}
+
+printNews ();
