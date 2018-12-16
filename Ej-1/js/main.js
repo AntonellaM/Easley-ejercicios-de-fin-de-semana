8d93cdef61bb621d745011a1ddfab78d02913b69
desigc <desi.gc93@gmail.com>
2018-12-16 12:44:26 +0100

    Retouch Ex1

--- Ej-1/js/main.js
+++ Ej-1/js/main.js
@@ -23,15 +23,15 @@ const data = [
   }
 ];
 
-const listEl = document.querySelector('news');
-const listContent = '';
+const listEl = document.querySelector('.news');
+let listContent = '';
 
 for (let i = 0; i < data.length; i++) {
 
-  listEl.innerHTML += `<li class="news__item">
+  listContent += `<li class="news__item">
     <h2 class="news__title">${data[i].title}</h2>
     <img class="news__image" src="${data[i].image}" alt="${data[i].title}">
     </li>`;
 
 }
-
+listEl.innerHTML = listContent;
