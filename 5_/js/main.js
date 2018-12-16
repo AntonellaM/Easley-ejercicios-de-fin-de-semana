7e8fb6c976f0bd01ebf79392c5e92da9039ab39a
Stephanie Guevara <stephanieguevaragullo@gmail.com>
2018-12-16 01:06:51 +0100

    fifth

--- 5_/js/main.js
+++ 5_/js/main.js
@@ -1,24 +1,38 @@
 'use strict';
+const content = document.querySelector('.content');
 
-const data = [
-  {
-    title: 'Asteroids 101',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Life on Mars',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Martians invade Earth',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Humans aren\'t real',
-    image: 'https://via.placeholder.com/200x100'
-  },
-  {
-    title: 'Space The Final Frontier',
-    image: 'https://via.placeholder.com/200x100'
-  }
-];
+const fetchFunction = () => {
+  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+    .then(resluts => resluts.json())
+    .then(data => {
+      const palette = data.palettes;
+      const paletteName = palette[0].name;
+
+      const contentTitle = document.createElement('div');
+      const titleBox = document.createElement('h2');
+      const titleText = document.createTextNode(paletteName);
+      contentTitle.classList.add('content-title');
+      titleBox.appendChild(titleText);
+      contentTitle.appendChild(titleBox);
+      content.appendChild(contentTitle);
+      const contentColor = document.createElement('div');
+      contentColor.classList.add('content-color');
+
+      //palette colors
+      const palleteColors = palette[0].colors;
+      for (let color of palleteColors) {
+        //console.log(color);
+
+        const colorBox = document.createElement('div');
+        colorBox.style.backgroundColor =`#${color}`;
+        colorBox.classList.add('colors-box');
+
+        contentColor.appendChild(colorBox);
+        content.appendChild(contentColor);
+        console.log(content);
+      }
+
+    });
+};
+
+fetchFunction();
