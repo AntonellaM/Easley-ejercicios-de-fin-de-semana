c2168422d88c67d9972fccb1b3d5df58cddf3850
chicainoportuna <paula.mara@gmail.com>
2018-12-16 10:05:00 +0100

    Js cercise 1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,13 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const ul = document.querySelector('.news');
+
+for (let i= 0; i<data.length;i++){
+	ul.innerHTML+=
+	`<li class="news__item">
+		<h2 class="news__title">${data[i].title}</h2>
+		<img class="news__image" src="${data[i].image}" alt="${data[i].title}">
+	</li>`;
+}
