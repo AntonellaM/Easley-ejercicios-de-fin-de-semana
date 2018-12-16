b8a2f9ae7a67ec136e7d8a99502979558233cf26
alhana-yo <beatriz.german.rengel@gmail.com>
2018-12-16 20:43:34 +0100

    Deleting comments and unuseful parts of code

--- Ej4-Ahora_me_ves/js/main.js
+++ Ej4-Ahora_me_ves/js/main.js
@@ -9,47 +9,50 @@ let newsLi = '';
 
 //Hacemos la petición de las noticias al servidor y las añadimos al HTML
 
-fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
+function drawNewsList() {
 
-    //alamcenamos el contenido de la key "news"
-    arrayNews = data.news;
+  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
 
-    console.log('arrayNews',arrayNews);
+      //almacenamos el contenido de la key "news"
+      arrayNews = data.news;
 
-    //pintamos los li, cada uno con una noticia
 
-    for (let i = 0; i < arrayNews.length; i++) {
+      //pintamos los li, cada uno con una noticia
+      for (let i = 0; i < arrayNews.length; i++) {
 
-      newsLi += `
+        newsLi += `
       <li class="news__item news__item--no-image-visible">
         <h2 class="news__title">${arrayNews[i].title}</h2>
         <img class="news__image" src="${arrayNews[i].image}" alt="${arrayNews[i].title}">
       </li>`;
 
-    }
+      }
 
-    list.innerHTML = newsLi;
+      list.innerHTML = newsLi;
 
-    //Buscamos todos los elementos con clase .news__item. Tenemos que hacer esta búsqueda dentro del .then, porque si la hacemos fuera, aun no se han pintado los li
-    let elements = document.querySelectorAll('.news__item');
+      //Buscamos todos los elementos con clase .news__item. Tenemos que hacer esta búsqueda dentro del .then, porque si la hacemos fuera, aun no se han pintado los li
+      let elements = document.querySelectorAll('.news__item');
 
-    for ( let i = 0; i < elements.length; i++) {
-      elements[i].addEventListener('click', showHideImage);
-    }
+      //Añadimos un listener a cada item 
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', showHideImage);
+      }
 
-  });
+    });
 
+}
 
+/** Función que quita o pone la clase que oculta la imagen */
 
 function showHideImage(ev) {
 
   let currentLi = ev.currentTarget;
 
-  //comprobamos si el li tiene o no la clase
+  //comprobamos si el li tiene o no la clase. Se puede hacer de tres formas
 
   /* Forma 1
   if (currentLi.className === 'news__item news__item--no-image-visible') {
@@ -61,7 +64,7 @@ function showHideImage(ev) {
   }
   */
 
-  
+
   /* Forma 2
   if (currentLi.classList.contains('news__item--no-image-visible')) {
     currentLi.classList.remove('news__item--no-image-visible');
@@ -71,12 +74,10 @@ function showHideImage(ev) {
   }
   */
 
+  /* Forma 3 */
+
   currentLi.classList.toggle('news__item--no-image-visible');
-  
+
 }
 
-
-
-//
-
-
+drawNewsList();
--- Ej6-Pintar_paletas_color/js/main.js
+++ Ej6-Pintar_paletas_color/js/main.js
@@ -1,46 +1,28 @@
 'use strict';
 /******************* EJERCICIO 6 *********************/
 
-fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
-  .then(response => response.json())
-  .then(data => {
+function drawPalettes () {
+  fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
+    .then(response => response.json())
+    .then(data => {
+      
+      const colorPalettes = data.palettes; // en cada posición de este array, se almacena un objeto con su titulo y su paleta de colores
+      
+      addPaletteList(colorPalettes);
     
-    const colorPalettes = data.palettes; // en cada posición de este array, se almacena un objeto con su titulo y su paleta de colores
+    });
 
-    /**Todo esto hace lo mismo que la línea anterior 
-     for (let i = 0; i < data.palettes.length; i++) {
-       colorPalette[i] = data.palettes[i];
-     }
-    */
-    let names = []; // cada posicion d este array corresponde al título de cada paleta
-    let colors = [];// cada posición de este array corresponde con los colores de una de las paletas de color
-    console.log('paletas',colorPalettes);
+}
 
-    //nos metemos dentro de cada paleta, y vamos almacenando: su nombre y sus colores
-    for (let i=0; i < colorPalettes.length; i++) {
-      names [i] = colorPalettes[i].name;
-      colors[i] = colorPalettes[i].colors;
-    }
-    console.log('nombres guardados dentro del fetch',names);
-    console.log('colores guardados dentro del fetch',colors);
-
-    addPaletteList(colorPalettes);
-    
-
-  });
-
-// console.log('colores guardados fuera del fetch',colors);
-
-
-/*Función que pinta los div que necesitamos, dependiendo de las paletas de colores que existan en el repo
-*le pasamos por parámetro un array de objetos paleta
+/*Función que pinta la lista de paletas.
+**Le pasamos por parámetro el array de objetos que hay en JSON que nos devuelve la petición
 */
 
-
 function addPaletteList (colorPalettes) {
+  
+  const sectionPalettes = document.querySelector('.palette');
   let title;
   let colors;
-  const sectionPalettes = document.querySelector('.palette');
   let titleAndWrapperElements;
   let palette; //cada cuadradito
 
@@ -51,11 +33,11 @@ function addPaletteList (colorPalettes) {
     palette = '';
 
     for (let j = 0; j < colors.length; j++) {
-      //Añadimos los div. Cada div pintara un color de la paleta
+      //Añadimos los cuadraditos con cada uno de los colores
       palette += `<div class="color__item" id="color${j+1}" style="background-color:#${colors[j]}"></div>`;
     }
 
-    //Añadimos los títulos y los wrapper (uno de cada por paleta)
+    //Añadimos los títulos y los wrapper (uno por cada paleta)
     titleAndWrapperElements += `
     <h2 class="palette__title">${title}</h2>
     <div class="colors__wrapper">
@@ -63,6 +45,9 @@ function addPaletteList (colorPalettes) {
     </div>`;
    
   }
+  
   sectionPalettes.innerHTML = titleAndWrapperElements;
   
 }
+
+drawPalettes();
