53c7fc2ed5fe048f9dff937805d91bc4c6d5d8ef
aroatdlt <aroatato@gmx.com>
2018-12-16 21:18:15 +0100

    Finish exercise 1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,24 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const listEl = document.querySelector('.news');
+let titleObj = '';
+let imageObj = '';
+
+for (const object of data){
+  titleObj = object.title;
+  imageObj = object.image;
+  let newsItem = document.createElement('li');
+  newsItem.className = "news__item";
+  let newsTitle = document. createElement('h2');
+  newsTitle.className = "news__title";
+  let newsTitleContent = document.createTextNode(`${titleObj}`);
+  newsTitle.appendChild(newsTitleContent);
+  let newsImage = document.createElement('img');
+  newsImage.className = "news__image";
+  newsImage.src = `${imageObj}`;
+  newsImage.setAttribute('alt', `${titleObj}`);
+  newsItem.append(newsTitle,newsImage);
+  listEl.appendChild(newsItem);
+}
