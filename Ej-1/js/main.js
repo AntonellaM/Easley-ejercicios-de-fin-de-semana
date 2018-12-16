6890d34572fb42374bea144974fc8a8ba8a2ceef
desigc <desi.gc93@gmail.com>
2018-12-16 12:34:11 +0100

    Ex1

---
+++ Ej-1/js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const data = [
+  {
+    title: 'Asteroids 101',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Life on Mars',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Martians invade Earth',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Humans aren\'t real',
+    image: 'https://via.placeholder.com/200x100'
+  },
+  {
+    title: 'Space The Final Frontier',
+    image: 'https://via.placeholder.com/200x100'
+  }
+];
+
+const listEl = document.querySelector('news');
+const listContent = '';
+
+for (let i = 0; i < data.length; i++) {
+
+  listEl.innerHTML += `<li class="news__item">
+    <h2 class="news__title">${data[i].title}</h2>
+    <img class="news__image" src="${data[i].image}" alt="${data[i].title}">
+    </li>`;
+
+}
+
