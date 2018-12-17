421fbf98d6cb5a8a14e81cc84097714e9a01f4e9
katia1802 <katia.rojas.sandoval@gmail.com>
2018-12-17 09:37:00 +0100

    Exercise 6

--- js/mainpalettes.js
+++ js/mainpalettes.js
@@ -17,21 +17,48 @@ function paintPalette (){
 
             for (let i=0; i<colorsArr.length; i++){
                 let color = colorsArr[i];
-                console.log (color);
+                // console.log (color);
 
-                containerEl.innerHTML+=`<li class="color__item" style="background-color:#${color}; height: 50px; width: 50px">hola</li`;
-
-
+                containerEl.innerHTML+=`<li class="color__item" style="background-color:#${color}; height: 50px; width: 50px"></li`;
             }
-
             }
-
-
         }
-        
-        
         )
-
 }
 
 paintPalette();
+
+const containerAllEl = document.querySelector('.containerAll');
+
+function paintAllPalletes () {
+    fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
+    .then (response => response.json())
+    .then (data=> {
+        const paletteArr = data.palettes;
+        console.log (paletteArr)
+
+        for (let i=0; i<paletteArr.length; i++){
+            const paletteArrColors = paletteArr[i].colors;
+            // console.log(paletteArrColors);
+
+            containerAllEl.innerHTML += `<li>${paletteArr[i].name}</li>`
+
+
+            for (let i=0; i<paletteArrColors.length; i++){
+                const color = paletteArrColors[i];
+
+                // console.log (color);
+
+                containerAllEl.innerHTML+=`<li class="color__item" style="background-color:#${color}; height: 50px; width: 50px"></li`;
+
+            }
+
+        }
+    })
+
+
+}
+
+paintAllPalletes ()
+
+
