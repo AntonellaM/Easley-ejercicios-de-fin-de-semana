765c3872b64109021b83760185883c2f68275e37
aroatdlt <aroatato@gmx.com>
2018-12-17 12:16:03 +0100

    Finish exercise 6

--- js/main.js
+++ js/main.js
@@ -84,20 +84,56 @@ let listEl = document.querySelector('.news');
 // };
 
 //Ejercicio 5 -------------------------------------------------------
-let totalColors = "";
+// let totalColors = "";
 
-fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+// fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
+//   .then(response => response.json())
+//   .then(results => {
+//     const totalObjets = results.palettes;
+//     for (const object of totalObjets) {
+//       totalColors = object.colors;
+//     };
+//     for (const colors of totalColors) {
+//       const fatherDiv = document.querySelector('.father_div');
+//       const divOfColor = document.createElement('div');
+//       divOfColor.className = 'div__content';
+//       fatherDiv.appendChild(divOfColor);
+//       divOfColor.style.backgroundColor = `#${colors}`;
+//     }
+//   });
+
+//Ejercicio 6 --------------------------------------------------------
+let totalColors = "";
+let newItemEl = "";
+let newTitleEl = "";
+let contentTitle = "";
+let nameFilm = "";
+let newDivEl = "";
+let newContainerDiv = "";
+listEl = document.querySelector('ul');
+
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
   .then(response => response.json())
   .then(results => {
-    const totalObjets = results.palettes;
-    for (const object of totalObjets) {
-      totalColors = object.colors;
-    };
-    for (const colors of totalColors) {
-      const fatherDiv = document.querySelector('.father_div');
-      const divOfColor = document.createElement('div');
-      divOfColor.className = 'div__content';
-      fatherDiv.appendChild(divOfColor);
-      divOfColor.style.backgroundColor = `#${colors}`;
-    }
-  });
+    const totalFilms = results.palettes;
+    //Creamos los elementos de la lista con su título correspondiente
+    for (const films of totalFilms) {
+      totalColors = films.colors;
+      nameFilm = films.name;
+      newItemEl = document.createElement('li');
+      newItemEl.className = "item__film";
+      newTitleEl = document.createElement('h2');
+      contentTitle = document.createTextNode(nameFilm);
+      newContainerDiv = document.createElement('div');
+      newContainerDiv.className = "container_div";
+      newTitleEl.appendChild(contentTitle);
+      newItemEl.append(newTitleEl, newContainerDiv);
+      listEl.appendChild(newItemEl);
+      //Queremos crear divs para pintarlos con los colores en cada li
+      for (const color of totalColors){
+        newDivEl = document.createElement('div');
+        newDivEl.style.backgroundColor = `#${color}`;
+        newDivEl.className = "div__content";
+        newContainerDiv.appendChild(newDivEl);
+      };
+    }});
