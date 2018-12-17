99a7b7454bb915eacf0ed1c9a93a0f0c972052ce
aroatdlt <aroatato@gmx.com>
2018-12-17 10:23:36 +0100

    Finish exercise 5

--- js/main.js
+++ js/main.js
@@ -54,31 +54,50 @@ let listEl = document.querySelector('.news');
 //   }
 // }
 
-//Ejercicio 3----------------------------------------
-fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+//Ejercicio 3 y 4----------------------------------------
+// fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/news.json')
+//   .then(response => response.json())
+//   .then(data => data.news)
+// for (const object of data) {
+//   let newsItem = document.createElement('li');
+//   newsItem.className = "news__item, news__item--no-image-visible";
+//   let newsTitle = document.createElement('h2');
+//   newsTitle.className = "news__title";
+//   let newsTitleContent = document.createTextNode(`${object.title}`);
+//   newsTitle.appendChild(newsTitleContent);
+//   let newsImage = document.createElement('img');
+//   newsImage.className = "news__image";
+//   newsImage.src = `${object.image}`;
+//   newsImage.setAttribute('alt', `${object.title}`);
+//   newsItem.append(newsTitle, newsImage);
+//   listEl.appendChild(newsItem);
+// }
+
+// let newsItem = document.querySelectorAll('li');
+// for (let i = 0; i < newsItem.length; i++){
+//   newsItem[i].addEventListener('click', handleImageDisplay)
+// };
+
+// function handleImageDisplay(event) {
+//   let selectedImage = event.currentTarget;
+//   selectedImage.classList.toggle('news__item--no-image-visible');
+// };
+
+//Ejercicio 5 -------------------------------------------------------
+let totalColors = "";
+
+fetch('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
   .then(response => response.json())
-  .then(data => data.news)
-for (const object of data) {
-  let newsItem = document.createElement('li');
-  newsItem.className = "news__item, news__item--no-image-visible";
-  let newsTitle = document.createElement('h2');
-  newsTitle.className = "news__title";
-  let newsTitleContent = document.createTextNode(`${object.title}`);
-  newsTitle.appendChild(newsTitleContent);
-  let newsImage = document.createElement('img');
-  newsImage.className = "news__image";
-  newsImage.src = `${object.image}`;
-  newsImage.setAttribute('alt', `${object.title}`);
-  newsItem.append(newsTitle, newsImage);
-  listEl.appendChild(newsItem);
-}
-
-let newsItem = document.querySelectorAll('li');
-for (let i = 0; i < newsItem.length; i++){
-  newsItem[i].addEventListener('click', handleImageDisplay)
-};
-
-function handleImageDisplay(event) {
-  let selectedImage = event.currentTarget;
-  selectedImage.classList.toggle('news__item--no-image-visible');
-};
+  .then(results => {
+    const totalObjets = results.palettes;
+    for (const object of totalObjets) {
+      totalColors = object.colors;
+    };
+    for (const colors of totalColors) {
+      const fatherDiv = document.querySelector('.father_div');
+      const divOfColor = document.createElement('div');
+      divOfColor.className = 'div__content';
+      fatherDiv.appendChild(divOfColor);
+      divOfColor.style.backgroundColor = `#${colors}`;
+    }
+  });
