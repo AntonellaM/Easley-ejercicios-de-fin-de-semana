5231d2da4e1ec480df798e2df2d93b34bf1c0d23
laurasanra <laura.san.rada@gmail.com>
2018-12-28 22:40:49 +0100

    finish exercise

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 'use strict';
 
 const newsList = document.querySelector('.news');
+let colorsContainer = document.querySelector('.colors-container');
+
+function showImg(event){
+  const clickedImg = event.currentTarget;
+  clickedImg.classList.toggle('news__item--no-image-visible');
+};
 
 function martianNews() {
   const martian = document.querySelectorAll('.news__item');
   for (let i = 0; i < martian.length; i++) {
+    martian[i].addEventListener('click', showImg);
     const martianItem = martian[i].firstChild.innerHTML;
-    console.log(martian[i]);
-    if (martianItem.includes('Mars') || martianItem.includes('Martian')) {
+      if (martianItem.includes('Mars') || martianItem.includes('Martian')) {
       martian[i].classList.add('news__item--from-mars');
     }
   }
-
 };
 
 function bringNews() {
@@ -23,6 +28,7 @@ function bringNews() {
         const newItem = document.createElement('li');
         newsList.appendChild(newItem);
         newItem.classList.add('news__item');
+        newItem.classList.add('news__item--no-image-visible');
     
         const newTitle = document.createElement('h2');
         newItem.appendChild(newTitle);
@@ -40,8 +46,44 @@ function bringNews() {
     })
 }
 
-bringNews();
+function showColors(){
+fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json')
 
+.then(response => response.json())
+.then(function colorBox(data){
+  const colorNumbers = data.palettes[0].colors;
+  for (const color of colorNumbers){
+    const newColorBox = document.createElement('div');
+    newColorBox.classList.add('color__item');
+    colorsContainer.appendChild(newColorBox);
+    newColorBox.setAttribute('style', `background-color:#${color}`);
+  }
+
+
+}
+)};
+
+function showMoreColors(){
+  fetch ('https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json')
+  .then(response => response.json())
+  .then(function showMore(data){
+    const colorPalettes = data.palettes; 
+    for(let i = 0; i < colorPalettes.length; i++){
+      const newPalettes = data.palettes[i].colors;
+      console.log(newPalettes);
+
+      for (const color of newPalettes){
+      const newColorBox = document.createElement('div');
+      newColorBox.classList.add('color__item');
+      colorsContainer.appendChild(newColorBox);
+      newColorBox.setAttribute('style', `background-color:#${color}`);
+
+   }
+ } } ) }
+
+showColors();
+bringNews();
+showMoreColors();
 
 
 
