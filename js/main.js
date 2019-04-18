e8d1d939633dab7d3286dfcc9ed4f73db2babb58
antonellam <antonella.michelli@gmail.com>
2019-04-18 22:20:55 +0200

    First exercise

--- js/main.js
+++ js/main.js
@@ -22,3 +22,23 @@ const data = [
     image: 'https://via.placeholder.com/200x100'
   }
 ];
+
+const ulEl = document.querySelector('.news');
+
+for (const newsItem of data) {
+  const liEl = document.createElement('li');
+  const titleEl = document.createElement('h2');
+  const imgEl = document.createElement('img');
+  const text = document.createTextNode(newsItem.title);
+
+  liEl.classList.add('news__item');
+  titleEl.classList.add('news__title');
+  imgEl.classList.add('news__image')
+  imgEl.src = newsItem.image;
+  imgEl.alt = newsItem.title;
+
+  titleEl.appendChild(text);
+  liEl.appendChild(titleEl);
+  liEl.appendChild(imgEl);
+  ulEl.appendChild(liEl);
+}
